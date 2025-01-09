fb5139807056b50885c23e697c845a6dd4ee6c15
Beazzyk <dominik1504@tlen.pl>
2025-01-09 12:03:23 +0100

    Poprawka

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // vite.config.ts
 import { defineConfig } from 'vite'
 
@@ -11,17 +10,3 @@ export default defineConfig({
     port: 3000,  // Można wybrać port
   },
 })
-=======
-// vite.config.ts
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  root: './',  // Ścieżka do głównego folderu (gdzie znajduje się App.tsx i main.tsx)
-  build: {
-    outDir: 'dist',  // Folder, do którego będą generowane pliki podczas budowania
-  },
-  server: {
-    port: 3000,  // Można wybrać port
-  },
-})
->>>>>>> 2c4eef8a8c2af760e670169e3438d8a3a9301b0f
